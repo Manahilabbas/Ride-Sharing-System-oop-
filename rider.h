ecbbbb2845c9c6461b02211c8f12488e35de34e2
Manahil Abbas <manahilabbas645@gmail.com>
2025-05-13 11:44:09 +0500

    Update rider.h

--- rider.h
+++ rider.h
@@ -9,8 +9,9 @@ protected:
     int rideHistory;
 public:
     Rider();
-    Rider(string id, string n, string p);
-    void displayInfo() override;
+    Rider(string id, string n, string p,string pass);
+    //void displayInfo() override;
+    friend ostream& operator<<(ostream& out, const Rider& rider);
     void incrementRideHistory();
     void signUp() override;
 };
