9fbefd1b646137aa0d6706c60ed020c3f49a62fa
Manahil Abbas <manahilabbas645@gmail.com>
2025-05-13 11:37:16 +0500

    Update auth_manager.h

--- auth_manager.h
+++ auth_manager.h
@@ -1,16 +1,26 @@
 #ifndef AUTH_MANAGER_H
 #define AUTH_MANAGER_H
-
+#include "driver.h"
+#include "premium_rider.h"
+#include "user.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <conio.h>
 #include <map>
 #include <vector>
 #include <string>
-#include "user.h"
+
 
 class AuthManager {
 public:
+    
+    static string validatePassword();
     static bool isRegistered(const std::string& id);
-    static User* signUp(std::map<std::string, std::string>& credentials, std::vector<User*>& users, bool isDriver);
+   // static User* signUp(std::map<std::string, std::string>& credentials, std::vector<User*>& users, bool isDriver);
     static User* login(std::map<std::string, std::string>& credentials, const std::vector<User*>& users);
+    User* login(map<string, string>& credentials, vector<User*>& users);
 };
+bool isPhoneRegistered(const string& phone) ;
 
 #endif
