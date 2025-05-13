bcd7bc81289bafa6a3796e711bdb05549284e571
Manahil Abbas <manahilabbas645@gmail.com>
2025-05-13 11:45:52 +0500

    Update user.h

--- user.h
+++ user.h
@@ -6,15 +6,23 @@ using namespace std;
 
 class User {
 protected:
-    string userID, name, phoneNum, role;
+    string userID, name, phoneNum, role,password,Email;
 public:
     User();
-    User(string id, string n, string p, string r);
+    User(string id, string n, string p, string r,string passs);
     virtual void signUp();
-    virtual void displayInfo();
+    //virtual void displayInfo();
     string getName() const;
     string getID() const;
     string getRole() const;
+    string getPassword()const;
+    // Add these setters to your User class
+void setPhoneNum(const string& e) { phoneNum = e; }
+void setEmail(const string& e) { Email= e; }
+void setPassword(const string& p) { password = p; }
+void setRole(const string& r) { role = r; } // optional if role is already known
+
+    friend ostream& operator<<(ostream& out, const User& user);
 };
 
 #endif
