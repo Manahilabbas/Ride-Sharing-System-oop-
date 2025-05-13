88d365f85ee67baf79caa5bc6ce72ff6677e8311
Manahil Abbas <manahilabbas645@gmail.com>
2025-05-13 11:43:12 +0500

    Update ride.h

--- ride.h
+++ ride.h
@@ -1,16 +1,26 @@
 #ifndef RIDE_H
 #define RIDE_H
 
-#include <map>
+#include <iostream>
+#include <vector>
 #include <string>
+#include <map>
+
 using namespace std;
+
+struct ZonePoint {
+    string name;
+    double x, y;
+};
 
 class Ride {
 public:
-    static map<string, int> zoneIndices;
-    static map<pair<string, string>, double> distances;
-    static void displayZones();
-    static double calculateFare(string src, string dest, string vehicleType);
+ 
+
+    static double calculateFareFromCoords(const ZonePoint& src, const ZonePoint& dest, const string& vehicleType, double discount = 0.0);
+    static vector<ZonePoint> loadZonePoints(const string& filename);
+    static ZonePoint selectPoint(const vector<ZonePoint>& points, const string& prompt);
+    friend ostream& operator<<(ostream& out, const Ride& ride);
 };
 
 #endif
