ac2e76797ce5ba9ac1f8aaedc4ebebd5147956e0
Manahil Abbas <manahilabbas645@gmail.com>
2025-05-13 11:39:51 +0500

    Update premium_rider.cpp

--- premium_rider.cpp
+++ premium_rider.cpp
@@ -1,15 +1,15 @@
 #include "premium_rider.h"
+#include "subscription.h" // Include the Subscription header
 using namespace std;
 
 PremiumRider::PremiumRider() {}
 
-PremiumRider::PremiumRider(string id, string n, string p) : Rider(id, n, p) {}
-
-void PremiumRider::displayInfo() {
-    Rider::displayInfo();
-    plan.displaySubscription();
-}
+PremiumRider::PremiumRider(string id, string n, string p,string pass,Subscription&  plan) : Rider(id, n, p,pass),plan(plan) {}
 
 void PremiumRider::incrementRideHistory() {
     Rider::incrementRideHistory();
 }
+Subscription PremiumRider::getSubscription() const {
+    return plan;
+}
+
