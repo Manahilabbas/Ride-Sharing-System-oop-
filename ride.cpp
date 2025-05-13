f9be9c3d0db7dcc3fcbb2652abccae61f8b308c5
Manahil Abbas <manahilabbas645@gmail.com>
2025-05-13 11:42:02 +0500

    Update ride.cpp

--- ride.cpp
+++ ride.cpp
@@ -1,36 +1,70 @@
 #include "ride.h"
+#include <fstream>
+#include <sstream>
+#include <cmath>
 #include <iostream>
+
 using namespace std;
 
-map<string, int> Ride::zoneIndices = {
-    {"Saddar", 0}, {"DHA", 1}, {"Clifton", 2}, {"Gulshan-e-Iqbal", 3},
-    {"PECHS", 4}, {"North Karachi", 5}, {"Nazimabad", 6}, {"Gulberg", 7},
-    {"Malir", 8}, {"Korangi", 9}
-};
 
-map<pair<string, string>, double> Ride::distances = {
-    {{"Saddar", "DHA"}, 10}, {{"Saddar", "Clifton"}, 5}, {{"DHA", "Gulshan-e-Iqbal"}, 18},
-    {{"Clifton", "Gulshan-e-Iqbal"}, 15}, {{"PECHS", "North Karachi"}, 14},
-    {{"Saddar", "North Karachi"}, 20}, {{"Clifton", "Korangi"}, 16},
-    {{"Malir", "Nazimabad"}, 22}, {{"Korangi", "Gulshan-e-Iqbal"}, 13},
-    {{"DHA", "North Karachi"}, 25}, {{"Nazimabad", "Gulberg"}, 6},
-    {{"PECHS", "Malir"}, 12}, {{"Saddar", "PECHS"}, 4}, {{"Gulberg", "Korangi"}, 17}
-};
 
-void Ride::displayZones() {
-    cout << "\nAvailable Zones:\n";
-    for (const auto& z : zoneIndices) {
-        cout << "- " << z.first << endl;
+map<string, int> Ride::zoneIndices;
+map<pair<string, string>, int> Ride::zoneMap;
+
+
+vector<ZonePoint> Ride::loadZonePoints(const string& filename) {
+    vector<ZonePoint> points;
+    ifstream file(filename);
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string zonePart, coordPart;
+        getline(ss, zonePart, ':');
+        getline(ss, coordPart);
+
+        size_t comma = coordPart.find(',');
+        if (comma != string::npos) {
+            double x = stod(coordPart.substr(0, comma));
+            double y = stod(coordPart.substr(comma + 1));
+            points.push_back({zonePart, x, y});
+        }
     }
+    return points;
 }
 
-double Ride::calculateFare(string src, string dest, string vehicleType) {
-    pair<string, string> key1 = {src, dest}, key2 = {dest, src};
-    double baseRate = 30.0;
-    double distance = distances.count(key1) ? distances[key1] : distances[key2];
-    double multiplier = (vehicleType == "Bike") ? 1.0 :
-                        (vehicleType == "Rickshaw") ? 1.2 :
-                        (vehicleType == "Non-AC Car") ? 1.5 :
-                        (vehicleType == "AC Car") ? 2.0 : 1.0;
-    return distance * baseRate * multiplier;
+ZonePoint Ride::selectPoint(const vector<ZonePoint>& points, const string& prompt) {
+    cout << "\n" << prompt << "\n";
+    for (size_t i = 0; i < points.size(); ++i) {
+        cout << i + 1 << ". " << points[i].name << " (" << points[i].x << "," << points[i].y << ")\n";
+    }
+    int choice;
+    cout << "Enter choice: ";
+    cin >> choice;
+    return points[choice - 1];
+}
+
+double Ride::calculateFareFromCoords(const ZonePoint& src, const ZonePoint& dest, const string& vehicleType, double discount) {
+    // Calculate Euclidean distance
+    double distance = sqrt(pow(dest.x - src.x, 2) + pow(dest.y - src.y, 2));
+
+    // Set rate based on vehicle type
+    double rate;
+    if (vehicleType == "Bike") rate = 10;
+    else if (vehicleType == "Rickshaw") rate = 15;
+    else if (vehicleType == "Non-AC Car") rate = 20;
+    else if (vehicleType == "AC Car") rate = 25;
+    else {
+        cout << "❌ Invalid vehicle type!\n";
+        return -1;
+    }
+
+    // Calculate fare
+    double fare = distance * rate;
+
+    // Apply discount if any
+    if (discount > 0) {
+        fare -= fare * (discount / 100.0);
+    }
+
+    return fare;
 }
