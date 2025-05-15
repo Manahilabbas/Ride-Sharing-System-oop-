0d0ed90eb34023d6596291ad355f807cc4aab3fd
armeenazam <ayanazam1976@gmail.com>
2025-05-15 21:43:12 +0500

    Update driver.h

--- driver.h
+++ driver.h
@@ -9,26 +9,26 @@ class Driver : public User {
 private:
     bool available;
     string vehicleType;
-    DriverReview review;
     string currentZone;
-    vector<DriverReview> driverReviews;
+    DriverReview review;
+    std::vector<DriverReview> driverReviews;
 
 public:
     Driver();
-    Driver(string id, string n, string p,string pass);
+    Driver(string id, string n, string p, string pass);
+
     void signUp() override;
     void askAvailability();
-    //void displayInfo() override;
     void addReview(const DriverReview& review);
-    friend ostream& operator<<(ostream& out, const Driver& driver);
-    
+    void displayReviews() const;
+
     string getCurrentZone() const;
     bool isAvailable() const;
     string getVehicleType() const;
     string getTimestamp() const;
-   
-    void displayReviews()const;
     string setTimestamp();
+
+    friend std::ostream& operator<<(std::ostream& out, const Driver& driver);
 };
 
-#endif
+#endif // DRIVER_H
